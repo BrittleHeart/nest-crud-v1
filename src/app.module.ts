bd642cb29dc9be91eaa62b2c98ff980a72d4e2ca
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-11 16:38:23 +0100

    Installed global middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import * as cors from 'cors';
+import * as helmet from 'helmet'
+import * as morgan from 'morgan';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors(), helmet(), morgan('dev')).forRoutes('/')
+  }
+}
