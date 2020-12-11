314b95ffa2a8897c295b2ab6596b3c1aeb4fd3fc
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-11 16:28:50 +0100

    Spinned dotenv with app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './middlewares/logger.middleware';
-import { TestMiddleware } from './middlewares/test-middleware.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(TestMiddleware, LoggerMiddleware)
-      .exclude(
-        {path: 'cats/:id', method: RequestMethod.GET},
-      )
-      .forRoutes('cats')
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+  dotenv.config()
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
