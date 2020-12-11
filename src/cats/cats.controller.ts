22ba7b353a93aed2db94066718ff63d813107dcb
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-11 16:21:18 +0100

    Prepering projekt for crud implementation :notes:

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,25 +1,7 @@
-import { Body, Controller, Delete, ForbiddenException, Get, Param, Post, Put, Req } from '@nestjs/common';
-import { CreateCatDto } from './create-cat.dto';
-import { Cat } from './cat.interface'
+import { Controller } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
     constructor(private readonly catsService: CatsService) {}
-
-    @Get()
-    async findAll(): Promise<Cat[]> {
-        return this.catsService.findAll();
-    }
-
-    @Get(':name')
-    async findOne(@Param('name') name: string): Promise<Cat> {
-        return this.catsService.findOne(name)
-    }
-
-    @Post()
-    async create(@Body() createCatDto: CreateCatDto): Promise<void> {
-        return this.catsService.create(createCatDto)
-    }
-
 }
