021ea2a772e3a0e3e4604ebb0269b09e1c1f329b
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-11 16:22:56 +0100

    Cleaning :poop:

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Cat } from './cat.interface';
 
 @Injectable()
 export class CatsService {
-    private readonly cats: Cat[] = [
-        {
-            name: "Squash",
-            age: 2,
-            breed: "cat"
-        }
-    ]
 
-    create(cat: Cat): void {
-        this.cats.push(cat)
-    }
-
-    findAll(): Cat[] {
-        return this.cats
-    }
-
-    findOne(name: string): Cat {
-        const cat = this.cats.find(cat => cat.name === name)
-        return cat
-    }
 }
--- src/middlewares/test-middleware.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-
-@Injectable()
-export class TestMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: Function) {
-    console.log('User has passed the middleware checker');
-    next();
-  }
-}
