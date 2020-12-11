dafb05219aecd41fea6e79024c122fb556511cd6
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-11 15:04:36 +0100

    Created tests middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { TestMiddleware } from './middlewares/test-middleware.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TestMiddleware).forRoutes('cats')
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
