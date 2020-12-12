3ed26843377b1e2cc536b231bbeda1bb2f99e417
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-12 18:02:37 +0100

    Modified login route controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import {Request} from 'express';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
     @UseGuards(LocalAuthGuard)
     @Post('login')
     @HttpCode(200)
     async login(@Req() request: Request): Promise<any> {
         // Passport automatically creates user object attached to request
-        return request.user;
+        return this.authService.login(request.user)
     }
 }
