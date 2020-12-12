85860777889b115e42dacd0dae309c801513248f
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-12 14:45:11 +0100

    Created hard-coded user's data :heart;

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            firstName: 'Bartosz Pazdur',
+            name: 'HiImNoshi',
+            password: 'test'
+        },
+        {
+            userId: 2,
+            firstName: 'Aleksander Wielki',
+            name: 'Creqy',
+            password: 'test2'
+        },
+        {
+            userId: 3,
+            firstName: 'Władysław Łokietek',
+            name: 'Mishin',
+            password: 'test3'
+        },
+    ];
+
+    async findOne(name: string): Promise<User | undefined> {
+        return this.users.find(user => user.name === name)
+    }
+}
