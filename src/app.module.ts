26c370bca43bf88e1c4e8970a873cb795b887ac7
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-12 14:34:57 +0100

    Created auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,11 @@ import * as helmet from 'helmet'
 import * as morgan from 'morgan';
 import { Connection } from 'typeorm';
 import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), CatsModule],
+  imports: [TypeOrmModule.forRoot(), CatsModule, AuthModule, UsersModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
