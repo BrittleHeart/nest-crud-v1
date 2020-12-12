6ef9acab58911c70d1602f9c81fc4675447e123e
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-12 16:08:41 +0100

    Moved local auth guard to seperate file :smile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import {Request} from 'express';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-    @UseGuards(AuthGuard('local'))
+    @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Req() request: Request): Promise<any> {
         // Passport automatically creates user object attached to request
