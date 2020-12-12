26b3aa49733a01b2f2c70d833d0ba7c1e303d01e
Bartosz Pazdur <b.pazdur@onet.pl>
2020-12-12 18:18:52 +0100

    Created jwt-auth.guard :rocket:

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import {Request} from 'express';
 import { AuthService } from './auth.service';
+import { JWTAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -13,5 +14,11 @@ export class AuthController {
     async login(@Req() request: Request): Promise<any> {
         // Passport automatically creates user object attached to request
         return this.authService.login(request.user)
+    }
+
+    @UseGuards(JWTAuthGuard)
+    @Get('profile')
+    getProfile(@Req() request: Request) {
+        return request.user
     }
 }
